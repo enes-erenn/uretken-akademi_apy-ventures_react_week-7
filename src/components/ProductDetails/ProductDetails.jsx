376e17a056f470837645ab87b757f59867b65521
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-05-07 19:26:54 +0300

    Added Select and Remove action types and added Product Details reducer

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,44 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import {
+  removeSelectedProduct,
+  selectedProduct,
+} from "../../redux/actions/ProductActions";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const product = useSelector((state) => state.product);
+  const { image, title, price, category, descirption } = product;
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+
+  const getProductDetails = async () => {
+    const product = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => {
+        console.log(err);
+      });
+    dispatch(selectedProduct(product.data));
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") getProductDetails();
+    return () => {
+      dispatch(removeSelectedProduct());
+    };
+  }, [productId]);
+
+  return (
+    <div>
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div>{title}</div>
+      )}
+    </div>
+  );
 };
 
 export default ProductDetails;
--- src/components/ProductWidget/ProductWidget.jsx
+++ src/components/ProductWidget/ProductWidget.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ProductWidget = () => {
   const products = useSelector((state) => state.allProducts.products);
-  /*   const { id, title } = products[0]; */
-  console.log(products);
-  return <div>{/* {title} */}</div>;
+  const renderProducts = products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div key={id}>
+        <Link to={`/product/${id}`}>{title}</Link>
+      </div>
+    );
+  });
+  return <>{renderProducts}</>;
 };
 
 export default ProductWidget;
