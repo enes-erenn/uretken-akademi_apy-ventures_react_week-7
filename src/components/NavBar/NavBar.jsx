bb080dbe131a34cfece9b20ad6af652938d3176e
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-05-07 18:38:35 +0300

    Created the Components

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return (
+    <navbar className={styles.container}>
+      <h3>UretkenShop</h3>
+    </navbar>
+  );
+};
+
+export default NavBar;
