c8ee12999b2e0d519b35f633ffb7478fbe94abb1
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-05-07 22:20:17 +0300

    Product Details  styles improved

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Rating from "@mui/material/Rating";
+import styles from "./ProductDetails.module.css";
 import { useParams } from "react-router-dom";
 import {
   removeSelectedProduct,
@@ -10,10 +12,10 @@ import {
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product);
-  const { image, title, price, category, descirption } = product;
+  const { image, title, price, category, description, rating } = product;
   const { productId } = useParams();
   const dispatch = useDispatch();
-
+  const [value, setValue] = React.useState(2);
   const getProductDetails = async () => {
     const product = await axios
       .get(`https://fakestoreapi.com/products/${productId}`)
@@ -33,9 +35,27 @@ const ProductDetails = () => {
   return (
     <div>
       {Object.keys(product).length === 0 ? (
-        <div>...Loading</div>
+        <div className={styles.loading}>...Loading</div>
       ) : (
-        <div>{title}</div>
+        <div className={styles.container}>
+          <div className={styles.box}>
+            <img src={image} className={styles.image} />
+            <div className={styles.productInfos}>
+              <h3>{title}</h3>
+              <h5>{category} </h5>
+              <Rating
+                name="simple-controlled"
+                value={rating.rate}
+                readOnly
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+              <p>{description}</p>
+              <span>${price}</span>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
